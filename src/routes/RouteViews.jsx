d6280a65872a25e5b4ae39fdb515fe73101e8bd0
nacho1706 <ignacioalbarracin15@gmail.com>
2024-05-20 21:15:02 -0300

    Se agregan los paquetes minimos nesesarios para el proyecto react-router-dom y react-bootstrap

---
+++ src/routes/RouteViews.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+
+const RouteViews = () => {
+  return (
+    <>
+    <Routes>
+        {/* IR AGREGANDO RUTAS */}
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+    </Routes>
+    </>
+  )
+}
+
+export default RouteViews
