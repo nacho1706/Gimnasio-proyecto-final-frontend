dc603ae146b2f9f3b877a007d6a851625a62d6fd
facundo parizzi <fakv_28@hotmail.com>
2024-06-25 00:09:47 -0300

    se creo contacto-navbar-footer

---
+++ src/component/NavbarP.jsx
@@ -0,0 +1,36 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../pages/style/NavbarP.css";
+
+const NavbarP = () => {
+  return (
+    <Navbar expand="lg" data-bs-theme="dark">
+      <Container fluid className="navbar px-5">
+        <Navbar.Brand href="#home">
+          <img
+            src="i_will_design_eye_catching_modern_sports__fitness_and_gym_logo-removebg-preview.png"
+            alt=""
+            width={120}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Servicio</Nav.Link>
+            <Nav.Link href="#link">Planes</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav className="ms-auto">
+              <Nav.Link href="#link">Iniciar Sesion</Nav.Link>
+              <Nav.Link href="#link">Registrarse</Nav.Link>
+            </Nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarP;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import "../pages/style/Contact.css";
+
+// Definimos el esquema de validación con Yup
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("El correo electrónico no es válido")
+    .required("El correo electrónico es requerido"),
+  message: yup
+    .string()
+    .required("El mensaje es requerido")
+    .min(10, "El mensaje debe tener al menos 10 caracteres"),
+});
+
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errasdors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // Aquí puedes manejar el envío del formulario, por ejemplo, enviándolo a una API
+  };
+
+  return (
+    <div className="contact-form-container mt-5 mb-5">
+      <h2>Contacto</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="email">Correo Electrónico</label>
+          <input id="email" type="email" {...register("email")} />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Mensaje</label>
+          <textarea id="message" {...register("message")} />
+        </div>
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
