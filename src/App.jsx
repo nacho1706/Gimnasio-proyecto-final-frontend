e36ba7762245dac5f156aceec9697d3735b2e4e5
facundo parizzi <fakv_28@hotmail.com>
2024-06-26 21:12:04 -0300

    se modifico

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import RouteViews from "./routes/RouteViews";
 import { BrowserRouter } from "react-router-dom";
 import NavbarP from "./component/NavbarP";
 import Footer from "./component/Footer";
+import Contact from "./pages/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +15,7 @@ function App() {
     <>
       <BrowserRouter>
         <NavbarP />
-        <RouteViews />
+        <Contact />
         <Footer />
       </BrowserRouter>
     </>
