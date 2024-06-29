59e3b177544626d906e4a53dbfb959a421b10e09
facundo parizzi <fakv_28@hotmail.com>
2024-06-28 22:23:11 -0300

    se creo pagina error 404

--- src/routes/RouteViews.jsx
+++ src/routes/RouteViews.jsx
@@ -1,21 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import HomeUser from "../pages/HomeUser"
-import Login from "../pages/Login"
-import Register from "../pages/Register"
+import { Route, Routes } from "react-router-dom";
+import HomeUser from "../pages/HomeUser";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFund";
 
 const RouteViews = () => {
   return (
     <>
       <Routes>
         {/* IR AGREGANDO RUTAS */}
-        <Route path="/homeUser" element={<HomeUser />}/>
-        <Route path="/Login" element={<Login />}/>
-        <Route path="/Register" element={<Register />}/>
-        <Route path="/Contact" element={<Contact />}/>
-        <Route/>
-        <Route/>
-    </Routes>
+        <Route path="/homeUser" element={<HomeUser />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/error-404" element={<NotFound />} />
+        <Route />
+        <Route />
+      </Routes>
     </>
   );
 };
